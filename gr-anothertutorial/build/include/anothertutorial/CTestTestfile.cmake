17b5584194f91cc2e8f0ba041314d09cc4edd543
LoCorpi3b <edgar.escalan@gmail.com>
2018-07-31 15:32:18 -0700

    removed build directory

--- gr-anothertutorial/build/include/anothertutorial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/locorpi3b/block_writing/gr-anothertutorial/include/anothertutorial
-# Build directory: /home/locorpi3b/block_writing/gr-anothertutorial/build/include/anothertutorial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gr-anothertutorial/build/lib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/locorpi3b/block_writing/gr-anothertutorial/lib
-# Build directory: /home/locorpi3b/block_writing/gr-anothertutorial/build/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
