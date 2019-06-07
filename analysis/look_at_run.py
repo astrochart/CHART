b241f3e4fcaf0f10786c81e9d6e69ededd9309f7
Adam Beardsley <adam.p.beardsley@gmail.com>
2019-06-07 09:49:42 -0700

    make some better variable names

--- analysis/look_at_run.py
+++ analysis/look_at_run.py
@@ -3,42 +3,42 @@ import matplotlib.pyplot as plt
 import glob
 import os
 
-dir = os.path.expanduser('~') + '/data/day2/'
-data_files = sorted(glob.glob(dir + '*dat'))
-meta_files = sorted(glob.glob(dir + '*metadata.npz'))
+path = os.path.expanduser('~') + '/data/day2/'
+data_files = sorted(glob.glob(path + '*dat'))
+meta_files = sorted(glob.glob(path + '*metadata.npz'))
 
 meta = np.load(meta_files[0])
-d = np.fromfile(data_files[0], dtype=np.float32)
-d = d.reshape(d.size / meta['vector_length'], meta['vector_length'])
+data = np.fromfile(data_files[0], dtype=np.float32)
+data = data.reshape(data.size / meta['vector_length'], meta['vector_length'])
 
 # Make waterfall for given cfreq for full night
 cfreq = 50e6
-d = []
-t = []
+data = []
+time = []
 for mf, df in zip(meta_files, data_files):
-	m = np.load(mf)
-	if m['frequency'] != cfreq:
+	meta = np.load(mf)
+	if meta['frequency'] != cfreq:
 		continue
-	t.append(m['times'][0])
+	time.append(meta['times'][0])
 	dtemp = np.fromfile(df, dtype=np.float32)
-	d.append(dtemp.reshape(dtemp.size / m['vector_length'],
-						   m['vector_length']))
+	data.append(dtemp.reshape(dtemp.size / meta['vector_length'],
+						   meta['vector_length']))
 
 
 # Make a waterfall by averaging each scan
-d = []
+data = []
 for mf, df in zip(meta_files, data_files):
-	m = np.load(mf)
+	meta = np.load(mf)
 	dtemp = np.fromfile(df, dtype=np.float32)
-	dtemp = dtemp.reshape(dtemp.size / m['vector_length'],
-						  m['vector_length'])
-	d.append(np.mean(dtemp, axis=0))
+	dtemp = dtemp.reshape(dtemp.size / meta['vector_length'],
+						  meta['vector_length'])
+	data.append(np.mean(dtemp, axis=0))
 	
 # break into even and odd frequency tunings
-d = np.array(d).reshape(48, 100, 1024)
+data = np.array(data).reshape(48, 100, 1024)
 # Flag center channels
-d[:, :, 511:514] = 0.0
+data[:, :, 511:514] = 0.0
 
-d0 = d[:, ::2, :].reshape(48, -1)
-d1 = d[:, 1::2, :].reshape(48, -1)
+data0 = data[:, ::2, :].reshape(48, -1)
+data1 = data[:, 1::2, :].reshape(48, -1)
 
