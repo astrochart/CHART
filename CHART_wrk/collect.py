be0e137da3d80648d1eec9af8d9c3f51eecbfb85
Adam Beardsley <adam.p.beardsley@gmail.com>
2019-08-26 10:53:51 -0700

    Take out unused stuff in collect.py, clean line breaks

--- CHART_wrk/collect.py
+++ CHART_wrk/collect.py
@@ -5,42 +5,39 @@
 # Title: Collectrtldata
 # Generated: Tue Aug 14 16:42:11 2018
 ##################################################
-#These are imports from GNU Radio 
+
 from gnuradio import blocks 
-from gnuradio import eng_notation #Engineering notation or engineering form is a version of scientific notation in which the exponent of ten must be divisible by three (i.e., they are powers of a thousand, but written as, for example, 106 instead of 10002).
-from gnuradio import fft #Fourier transform function.
-from gnuradio import gr #Need this to run GNU Radio.
-from gnuradio.eng_option import eng_option
+from gnuradio import fft
+from gnuradio import gr
 from gnuradio.fft import window
-from gnuradio.filter import firdes 
-from optparse import OptionParser #allows users to specify options in the conventional GNU/POSIX syntax, and additionally generates usage and help messages for you.
 import chart 
-import osmosdr #Lets you take advantage of a common software API in your application(s) independent of the underlying radio hardware.
-import time #Time function
-import datetime #supplies classes for manipulating dates and times in both simple and complex ways. 
-import timeit #Provides a simple way to time small bits of Python code.
-import numpy as np #Package for scientfic computing in python.
-import pprint #Module that provides a capability to "pretty print" arbitrary Python data structures in a well formatted and more readable way. 
-from ast import literal_eval # 'ast' helps Python applications to process trees of the Python abstract syntax grammar. 'literal_eval'Safely evaluate an expression node or a Unicode or Latin-1 encoded string containing a Python literal or container display. The string or node provided may only consist of the following Python literal structures: strings, numbers, tuples, lists, dicts, booleans, and None.
+import osmosdr
+import time
+import datetime
+import numpy as np
+import argparse
+
 
 #this is our collect data class with an instance of gnu radio first block
 class collectrtldata(gr.top_block):
-#we are creating a structure to our class and only contains center frequency
+    """Class to collect RTL data and metadata."""
+
     def __init__(self, c_freq):
-        gr.top_block.__init__(self, "Collectrtldata")  #does this loop???                                
+        gr.top_block.__init__(self, "Collectrtldata")
 
         ##################################################
         # Variables
         ##################################################
-        self.veclength = veclength = 1024 #manually sets the vector length
-        self.samp_rate = samp_rate = 2e6 #manually sets the sample rate
+        self.veclength = 1024 #manually sets the vector length
+        self.samp_rate = 2e6 #manually sets the sample rate
         self.int_length = 100 #manually sets the integration length
+        self.nint = 100 # Manually set number of integrations per file
         self.set_filename() #names the file
         ##################################################
         # Blocks
         ##################################################
-        self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
-        self.rtlsdr_source_0.set_sample_rate(samp_rate)
+        self.rtlsdr_source_0 = osmosdr.source(args="numchan=" + str(1) + " ")
+        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
         self.set_c_freq(c_freq)
         self.rtlsdr_source_0.set_freq_corr(0, 0)
         self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
@@ -52,45 +49,49 @@ class collectrtldata(gr.top_block):
         self.rtlsdr_source_0.set_antenna("", 0)
         self.rtlsdr_source_0.set_bandwidth(0, 0)
           
-        self.fft_vxx_0 = fft.fft_vcc(veclength, True, (window.blackmanharris(1024)), True, 1)
-        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_float*1, 1024)
-        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, veclength)
-        self.blocks_integrate_xx_0 = blocks.integrate_ff(self.int_length, veclength)
-        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, veclength*100*100)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*veclength, self.data_file, False)
+        self.fft_vxx_0 = fft.fft_vcc(self.veclength, True,
+                                     (window.blackmanharris(self.veclength)),
+                                     True, 1)
+        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_float,
+                                                                 self.veclength)
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex,
+                                                                 self.veclength)
+        self.blocks_integrate_xx_0 = blocks.integrate_ff(self.int_length,
+                                                         self.veclength)
+        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex,
+                                         self.veclength * self.int_length * self.nint)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float * veclength, 
+                                                   self.data_file, False)
         self.blocks_file_sink_0.set_unbuffered(False)
-        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(veclength)
-        self.chart_meta_trig_py_ff_0 =chart.meta_trig_py_ff(veclength)
+        self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(self.veclength)
+        self.chart_meta_trig_py_ff_0 = chart.meta_trig_py_ff(self.veclength)
         ##################################################
         # Connections
         ##################################################
-	#these are the lines that connect each block in the visual display of GNU Radio
-        self.connect((self.chart_meta_trig_py_ff_0, 0), (self.blocks_file_sink_0, 0))    
-        self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.blocks_integrate_xx_0, 0))    
+	    # These are the lines that connect each block in the visual display of GNU Radio
+        self.connect((self.rtlsdr_source_0, 0), (self.blocks_head_0, 0))    
         self.connect((self.blocks_head_0, 0), (self.blocks_stream_to_vector_0, 0))    
-        self.connect((self.blocks_integrate_xx_0, 0), (self.chart_meta_trig_py_ff_0, 0))    
         self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))    
         self.connect((self.fft_vxx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))    
-        self.connect((self.rtlsdr_source_0, 0), (self.blocks_head_0, 0))    
-
+        self.connect((self.blocks_complex_to_mag_squared_0, 0),
+                     (self.blocks_integrate_xx_0, 0))    
+        self.connect((self.blocks_integrate_xx_0, 0), (self.chart_meta_trig_py_ff_0, 0))    
+        self.connect((self.chart_meta_trig_py_ff_0, 0), (self.blocks_file_sink_0, 0))    
+        
+        # Get start time
         self.start_time = time.time()
-#retrieves the vector length
-    def get_veclength(self):
-        return self.veclength
-#
+
     def set_veclength(self, veclength):
         self.veclength = veclength
-        self.blocks_head_0.set_length(self.veclength*100*100)
-
-    def get_samp_rate(self):
-        return self.samp_rate
+        self.blocks_head_0.set_length(self.veclength * self.int_length * self.nint)
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
 
     def set_c_freq(self, c_freq, sleep=0.5):
-        """ Function to reset the tuning frequency
+        """Function to reset the tuning frequency.
+
         Args:
             c_freq: center frequency, in Hz
             sleep: Time to sleep to allow the radio to settle. Default 0.5 seconds.
@@ -104,7 +105,8 @@ class collectrtldata(gr.top_block):
         time.sleep(sleep)
 
     def set_filename(self, filename=None):
-        """ Function to set filename
+        """Function to set filename.
+        
         Args:
             filename: Optional filename. If not supplied, create filename from datetime
         """
@@ -121,21 +123,6 @@ class collectrtldata(gr.top_block):
         except AttributeError:
             pass
 
-    def parameters(self):
-        d={'date': str(datetime.date.today()),
-           'start_time': time.time(), 
-           'samp_rate': self.samp_rate,
-           'frequency': self.c_freq,
-           'vector_length': self.get_veclength(),
-           'int_length': self.int_length,
-           #data file (.dat file it refers to)
-           'data_file': self.data_file,
-           #print after every integration. 100 d for each c_freq
-           'metadata_file': self.metadata_file,
-           'times': self.chart_meta_trig_py_ff_0.get_l()
-          }
-        return d
-#assigning the meta data
     def meta_save(self):
         np.savez(self.metadata_file,
            date = str(datetime.date.today()),
@@ -143,15 +130,13 @@ class collectrtldata(gr.top_block):
            end_time = time.time(), 
            samp_rate = self.samp_rate,
            frequency = self.c_freq,
-           vector_length = self.get_veclength(),
+           vector_length = self.veclength,
            int_length = self.int_length,
-           #data file (.dat file it refers to)
            data_file = self.data_file,
-           #print after every integration. 100 d for each c_freq
            metadata_file = self.metadata_file,
            times = self.chart_meta_trig_py_ff_0.get_l())
 
-def main(top_block_cls=collectrtldata, options=None):
+def main(top_block_cls=collectrtldata):
     #create while loop and timer variable. while loop halts when timer variable
     #reaches certain value. tb.wait(10) added at end to create intervals of time
     dt = 30 * 60  # Time between scans, in seconds
