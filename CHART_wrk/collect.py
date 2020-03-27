ba58f9cc768f5c41954c518026087e65c7fbec44
LoCorpi3b <edgar.escalan@gmail.com>
2020-03-27 15:48:33 -0700

    make sure collect still runs

--- CHART_wrk/collect.py
+++ CHART_wrk/collect.py
@@ -57,7 +57,8 @@ class collectrtldata(gr.top_block):
             self.data_dir = os.getcwd()
         else:
             self.data_dir = data_dir
-        self.set_filename()  # names the file
+        # Initialize to null to avoid empty file
+        self.set_filename()
         ##################################################
         # Blocks
         ##################################################
@@ -131,12 +132,13 @@ class collectrtldata(gr.top_block):
             pass
         time.sleep(sleep)
 
-    def set_filename(self, filebase=None):
+    def set_filename(self, filebase=None, fullpath=None):
         """Set filename.
 
         Args:
             filebase: Optional base for filename. If not supplied,
                 create filename from datetime
+            fullpath: Optional full path for filename. Will override filebase.
         """
         if filebase is None:
             filebase = str(datetime.datetime.now()).replace(' ', '_')
@@ -180,11 +182,13 @@ def get_collect_args():
     ap.add_argument('--sleep_time', default=5., type=float, help='Sleep time '
                     'between checks for next scan time, in seconds. Default is 5.')
     ap.add_argument('--veclength', default=1024, type=int, help='Vector length '
-                    'for spectrum estimation. Default is 1024.')
-    ap.add_argument('--samp_rate', default=1., type=float, help='Sample rate '
-                    'of the radio, in MHz. Default is 1.')
+                    '(number of channels) for spectrum estimation. Default is 1024.')
+    ap.add_argument('--samp_rate', default=2., type=float, help='Sample rate '
+                    'of the radio, in MHz. Default is 2.')
     ap.add_argument('--int_length', default=100, type=int, help='Number of samples '
                     'per integration. Default is 100.')
+    ap.add_argument('--int_time', type=float, help='Integration time, in seconds.'
+                    ' Overrides the --int_length argument.')
     ap.add_argument('--nint', default=100, type=int, help='Number of integrations '
                     'per file. Default is 100.')
     ap.add_argument('--data_dir', default=None, type=str, help='Data directory. '
@@ -201,7 +205,10 @@ def get_collect_args():
     # Do a quick check on the data directory
     if args.data_dir is None:
         args.data_dir = os.getcwd()
+    else:
+        args.data_dir = os.path.expanduser(args.data_dir)
     if not os.path.isdir(args.data_dir):
+        bad_dir = args.data_dir
         args.data_dir = os.getcwd()
         warnings.warn(bad_dir + 'Data directory not valid, using cwd = ' + args.data_dir)
 
@@ -211,11 +218,23 @@ def get_collect_args():
 def main(top_block_cls=collectrtldata):
     """Create a topblock and loop over frequencies and scans."""
     args = get_collect_args()
-    scan_number = 0  # used as scan counter
+    if args.int_time is None:
+        int_time = args.veclength / args.samp_rate * args.int_length
+        print('int_length set to ' + str(args.int_length) + ' which corresonds'
+              'to integration time of ' + str(int_time) + ' seconds.')
+    else:
+        args.int_length = int(args.int_time * args.samp_rate / args.vec_length)
+        int_time = args.veclength / args.samp_rate * args.int_length
+        print('int_time set to ' + str(args.int_time) + ' seconds. Using '
+              'int_length of ' + str(args.int_length) + '. Actual integraton '
+              'time is ' + str(int_time) + ' seconds.') 
     tb = top_block_cls(c_freq=args.freq_i, veclength=args.veclength,
                        samp_rate=args.samp_rate, int_length=args.int_length,
                        nint=args.nint, data_dir=args.data_dir)
+    scan_number = 0  # used as scan counter
     t0 = time.time()
+    # Remove the empty file that was created when instantiating top block
+    os.remove(tb.data_file)
     while time.time() - t0 < args.total_time:
         for c_freq in np.arange(args.freq_i, args.freq_f, args.df):
             print('Frequency: ' + str(c_freq / 10**6) + ' MHz')
@@ -225,9 +244,9 @@ def main(top_block_cls=collectrtldata):
             tb.start()
             tb.wait()
             tb.meta_save()
-        i += 1
+        scan_number += 1
         while time.time() < t0 + scan_number * args.scan_period:
-            time.sleep(args.sleep_time)  # Sleep 5 seconds
+            time.sleep(args.sleep_time)  # Sleep before trying again
     del(tb)
 
 
