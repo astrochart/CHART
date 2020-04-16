da8413a9c3d58df9b0c74b4945b7042912d247b6
Adam Beardsley <adam.p.beardsley@gmail.com>
2020-04-16 13:14:29 -0700

    move bit of gps stuff into chart module

---
+++ src/chart/gps.py
@@ -0,0 +1,23 @@
+import gps
+
+
+def monitor():
+    # Listen on port 2947 (gpsd) of localhost
+    session = gps.gps('localhost', '2947')
+    session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+    while True:
+        try:
+            report = session.next()
+            print('\n')
+            print(report)
+            if report['class'] == 'TPV':
+                if hasattr(report, 'time'):
+                    print(report.time)
+        except KeyError:
+            pass
+        except KeyboardInterrupt:
+            quit()
+        except StopIteration:
+            session = None
+            print('GPSD has terminated')
