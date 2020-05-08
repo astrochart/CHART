4edb3cdb6dd637edc4ff2b294512c70a82bbbf39
Adam Beardsley <adam.p.beardsley@gmail.com>
2020-05-08 12:38:47 -0700

    Rename collect.py to freq_and_time_scan.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup_args = {
     'package_dir': {'chart': 'src/chart'},
     'packages': ['chart'],
     'include_package_data': True,
-    'scripts': ['daq/collect.py'],
+    'scripts': ['daq/freq_and_time_scan.py'],
     'version': 1.0,
     'install_requires': [
         'numpy>=1.10',
