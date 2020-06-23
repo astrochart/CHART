83c97cfc84bc53f32d38ba2c9c2368304a20a4cb
Lindsay Berkhout <34925577+lmberkhout@users.noreply.github.com>
2020-06-23 10:44:45 -0700

    Update blocks.py
    today I learned that if you change an import you have to setup install again for it to register your changes

--- src/chart/blocks.py
+++ src/chart/blocks.py
@@ -162,28 +162,16 @@ class TopBlock(gr.top_block):
 
     def meta_save(self):
         """Save the metadata."""
-        print str(self.metadata_file)
-        print np.asarray(str(datetime.date.today())
-        print np.asarray(self.start_time)
-        print np.asarray(time.time())
-        print np.asarray(self.samp_rate)
-        print np.asarray(self.c_freq)
-        print np.asarray(self.veclength)
-        print np.asarray(self.int_length)
-        print np.asarray(self.data_file)
-        print np.asarray(str(self.metadata_file))
-        print np.asarray(self.chart_meta_trig_py_ff_0.get_times())
-        print np.asarray(str(np.float32))
-        #np.savez(str(self.metadata_file),
-        #         data=np.asarray(str(datetime.date.today()),
-        #         start_time=np.asarray(self.start_time),
-        #         end_time=np.asarray(time.time()),
-        #         samp_rate=np.asarray(self.samp_rate),
-        #         frequency=np.asarray(self.c_freq),
-        #         vector_length=np.asarray(self.veclength),
-        #         int_length=np.asarray(self.int_length),
-        #         data_file=np.asarray(self.data_file),
-        #         metadata_file=np.asarray(str(self.metadata_file)),
-        #         times=np.asarray(self.chart_meta_trig_py_ff_0.get_times()),
-        #         dtype=np.asarray('np.float32'),
+        np.savez(str(self.metadata_file),
+                 data=np.asarray(str(datetime.date.today()),
+                 start_time=np.asarray(self.start_time),
+                 end_time=np.asarray(time.time()),
+                 samp_rate=np.asarray(self.samp_rate),
+                 frequency=np.asarray(self.c_freq),
+                 vector_length=np.asarray(self.veclength),
+                 int_length=np.asarray(self.int_length),
+                 data_file=np.asarray(self.data_file),
+                 metadata_file=np.asarray(str(self.metadata_file)),
+                 times=np.asarray(self.chart_meta_trig_py_ff_0.get_times()),
+                 dtype=np.asarray('np.float32'),
                 )
