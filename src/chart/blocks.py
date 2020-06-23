46502975148a07f6d3fc59ad3f1ef7c6f360b147
Lindsay Berkhout <34925577+lmberkhout@users.noreply.github.com>
2020-06-23 09:40:21 -0700

    Update blocks.py
    still trying to fix the sitch

--- src/chart/blocks.py
+++ src/chart/blocks.py
@@ -162,16 +162,16 @@ class TopBlock(gr.top_block):
 
     def meta_save(self):
         """Save the metadata."""
-        np.savez(self.metadata_file,
-                 data=np.array(str(datetime.date.today()),
-                 start_time=np.array(self.start_time),
-                 end_time=np.array(time.time()),
-                 samp_rate=np.array(self.samp_rate),
-                 frequency=np.array(self.c_freq),
-                 vector_length=np.array(self.veclength),
-                 int_length=np.array(self.int_length),
-                 data_file=np.array(self.data_file),
-                 metadata_file=np.array(self.metadata_file),
-                 times=np.array(self.chart_meta_trig_py_ff_0.get_times()),
-                 dtype=np.array('np.float32'),
+        np.savez(str(self.metadata_file),
+                 data=np.asarray(str(datetime.date.today()),
+                 start_time=np.asarray(self.start_time),
+                 end_time=np.asarray(time.time()),
+                 samp_rate=np.asarray(self.samp_rate),
+                 frequency=np.asarray(self.c_freq),
+                 vector_length=np.asarray(self.veclength),
+                 int_length=np.asarray(self.int_length),
+                 data_file=np.asarray(self.data_file),
+                 metadata_file=np.asarray(str(self.metadata_file)),
+                 times=np.asarray(self.chart_meta_trig_py_ff_0.get_times()),
+                 dtype=np.asarray('np.float32'),
                 )
