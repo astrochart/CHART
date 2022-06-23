924e84bbc7303a25b15bd0722688fd63eb169015
Lindsey Arndt <lindsa@comcast.net>
2022-06-23 18:16:59 -0500

    change parameters

--- daq/gui.py
+++ daq/gui.py
@@ -8,12 +8,30 @@ import subprocess
 customtkinter.set_appearance_mode("Dark")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
+
+
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
 app.geometry("576x288")
 app.title("Today's Data")
 
-def stop():
-    os.system("exit")
+def parameters():
+    print("changing parameters")
+    param_app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    param_app.geometry("576x288")
+    param_app.title("Parameters")
+    
+    
+
+#def save(freq_i_in, freq_f_in, int_time_in, nint_in):
+   # print("saving the new parameters")
+   # print(freq_i_in)
+    
+     #(customtkinter.CTkEntry.get(freq_i_in), customtkinter.CTkEntry.get(freq_f_in), customtkinter.CTkEntry.get(int_time_in), )
+    #freq_i = customtkinter.CTkEntry.get(freq_i_in)
+    #freq_f = customtkinter.CTkEntry.get(freq_f_in)
+    #int_time = customtkinter.CTkEntry.get(int_time_in)
+    #nint = customtkinter.CTkEntry.get(nint_in)
+    #print(freq_i)
 
 def button_function():
     user = customtkinter.CTkEntry.get(uName)
@@ -40,9 +58,24 @@ def button_function():
     
     dirsUse = str(data_directory)+'/'+str(directory)
     print(dirsUse)
-   
-  
-    sCopy = 'python freq_and_time_scan.py --freq_i=1390 --freq_f=1450 --int_time=0.5 --nint=20 --data_dir='+dirsUse
+    
+    
+    print("saving the new parameters")
+    print(freq_i_in)
+    
+     #(customtkinter.CTkEntry.get(freq_i_in), customtkinter.CTkEntry.get(freq_f_in), customtkinter.CTkEntry.get(int_time_in), )
+    freq_i = customtkinter.CTkEntry.get(freq_i_in)
+    freq_f = customtkinter.CTkEntry.get(freq_f_in)
+    int_time = customtkinter.CTkEntry.get(int_time_in)
+    nint = customtkinter.CTkEntry.get(nint_in)
+    
+    print(freq_i)
+    
+    print(freq_i)
+    
+    sCopy = 'python freq_and_time_scan.py --freq_i='+freq_i+' --freq_f='+freq_f+' --int_time='+int_time+' --nint='+nint+' --data_dir='+dirsUse
+
+    #sCopy = 'python freq_and_time_scan.py --freq_i=1390 --freq_f=1450 --int_time=0.5 --nint=20 --data_dir='+dirsUse
     os.system(sCopy)
     
     #cmd = "python freq_and_time_scan.py --freq_i=1390 --freq_f=1450 --int_time=0.5 --nint=20 --data_dir --"+main_dir
@@ -53,6 +86,96 @@ def button_function():
 
 def combobox_callback(choice):
     print("combobox dropdown clicked:", choice)
+
+
+
+label = customtkinter.CTkLabel(master=app,
+                               text="Parameters:",
+                               width=100,
+                               height=25,
+                               fg_color=("white", "orange"),
+                               corner_radius=5)
+label.place(relx=0.2, rely=0.1, anchor=tkinter.W)
+
+
+
+label = customtkinter.CTkLabel(master=app,
+                               text="Initial Frequency:",
+                               width=100,
+                               height=25,
+                               fg_color=("white", "gray"),
+                               corner_radius=5)
+label.place(relx=0.1, rely=0.2, anchor=tkinter.W)
+##n, ne, e, se, s, sw, w, nw, or center
+
+freq_i_in = customtkinter.CTkEntry(master=app,
+                               placeholder_text="1390",
+                               width=60,
+                               height=25,
+                               border_width=2,
+                               corner_radius=10)
+freq_i_in.place(relx=0.3, rely=0.2, anchor=tkinter.W)
+    
+label = customtkinter.CTkLabel(master=app,
+                               text="Final Frequency:",
+                               width=100,
+                               height=25,
+                               fg_color=("white", "gray"),
+                               corner_radius=5)
+label.place(relx=0.1, rely=0.3, anchor=tkinter.W)
+##n, ne, e, se, s, sw, w, nw, or center
+
+freq_f_in = customtkinter.CTkEntry(master=app,
+                               placeholder_text="1450",
+                               width=60,
+                               height=25,
+                               border_width=2,
+                               corner_radius=10)
+freq_f_in.place(relx=0.3, rely=0.3, anchor=tkinter.W)
+    
+label = customtkinter.CTkLabel(master=app,
+                               text="Integration Time:",
+                               width=100,
+                               height=25,
+                               fg_color=("white", "gray"),
+                               corner_radius=5)
+label.place(relx=0.1, rely=0.4, anchor=tkinter.W)
+##n, ne, e, se, s, sw, w, nw, or center
+
+int_time_in = customtkinter.CTkEntry(master=app,
+                               placeholder_text="0.5",
+                               width=60,
+                               height=25,
+                               border_width=2,
+                               corner_radius=10)
+int_time_in.place(relx=0.3, rely=0.4, anchor=tkinter.W)
+    
+label = customtkinter.CTkLabel(master=app,
+                               text="Number of Integrations:",
+                               width=100,
+                               height=25,
+                               fg_color=("white", "gray"),
+                               corner_radius=5)
+label.place(relx=0.07, rely=0.5, anchor=tkinter.W)
+##n, ne, e, se, s, sw, w, nw, or center
+
+nint_in = customtkinter.CTkEntry(master=app,
+                               placeholder_text="20",
+                               width=60,
+                               height=25,
+                               border_width=2,
+                               corner_radius=10)
+nint_in.place(relx=0.34, rely=0.5, anchor=tkinter.W)
+    
+    
+    #saving = ((customtkinter.CTkEntry.get(freq_i_in)), (customtkinter.CTkEntry.get(freq_f_in)), (customtkinter.CTkEntry.get(int_time_in)), (customtkinter.CTkEntry.get(nint_in)))
+#button = customtkinter.CTkButton(master=app, text="Save", command=save)
+#button.place(relx=0.5, rely=.8, anchor=tkinter.CENTER)
+
+
+
+
+
 
 # Use CTkButton instead of tkinter Button
 button = customtkinter.CTkButton(master=app, text="Start", command=button_function)
@@ -65,7 +188,7 @@ label = customtkinter.CTkLabel(master=app,
                                height=25,
                                fg_color=("white", "gray"),
                                corner_radius=5)
-label.place(relx=0.3, rely=0.1, anchor=tkinter.W)
+label.place(relx=0.5, rely=0.2, anchor=tkinter.W)
 ##n, ne, e, se, s, sw, w, nw, or center
 
 uName = customtkinter.CTkEntry(master=app,
@@ -74,7 +197,7 @@ uName = customtkinter.CTkEntry(master=app,
                                height=25,
                                border_width=2,
                                corner_radius=10)
-uName.place(relx=0.5, rely=0.1, anchor=tkinter.W)
+uName.place(relx=0.7, rely=0.2, anchor=tkinter.W)
 
 
 label = customtkinter.CTkLabel(master=app,
@@ -83,7 +206,7 @@ label = customtkinter.CTkLabel(master=app,
                                height=25,
                                fg_color=("white", "gray"),
                                corner_radius=5)
-label.place(relx=0.3, rely=0.2, anchor=tkinter.W)
+label.place(relx=0.5, rely=0.3, anchor=tkinter.W)
 ##n, ne, e, se, s, sw, w, nw, or center
 
 dName = customtkinter.CTkEntry(master=app,
@@ -92,7 +215,7 @@ dName = customtkinter.CTkEntry(master=app,
                                height=25,
                                border_width=2,
                                corner_radius=10)
-dName.place(relx=0.5, rely=0.2, anchor=tkinter.W)
+dName.place(relx=0.7, rely=0.3, anchor=tkinter.W)
 
 label = customtkinter.CTkLabel(master=app,
                                text="Location:",
@@ -100,7 +223,7 @@ label = customtkinter.CTkLabel(master=app,
                                height=25,
                                fg_color=("white", "gray"),
                                corner_radius=5)
-label.place(relx=0.3, rely=0.3, anchor=tkinter.W)
+label.place(relx=0.5, rely=0.4, anchor=tkinter.W)
 ##n, ne, e, se, s, sw, w, nw, or center
 
 locName = customtkinter.CTkEntry(master=app,
@@ -109,7 +232,7 @@ locName = customtkinter.CTkEntry(master=app,
                                height=25,
                                border_width=2,
                                corner_radius=10)
-locName.place(relx=0.5, rely=0.3, anchor=tkinter.W)
+locName.place(relx=0.7, rely=0.4, anchor=tkinter.W)
 
 label = customtkinter.CTkLabel(master=app,
                                text="Trial:",
@@ -117,7 +240,7 @@ label = customtkinter.CTkLabel(master=app,
                                height=25,
                                fg_color=("white", "gray"),
                                corner_radius=5)
-label.place(relx=0.3, rely=0.4, anchor=tkinter.W)
+label.place(relx=0.5, rely=0.5, anchor=tkinter.W)
 ##n, ne, e, se, s, sw, w, nw, or center
 
 tName = customtkinter.CTkEntry(master=app,
@@ -126,7 +249,7 @@ tName = customtkinter.CTkEntry(master=app,
                                height=25,
                                border_width=2,
                                corner_radius=10)
-tName.place(relx=0.5, rely=0.4, anchor=tkinter.W)
+tName.place(relx=0.7, rely=0.5, anchor=tkinter.W)
 
 label = customtkinter.CTkLabel(master=app,
                                text="Time:",
@@ -134,16 +257,16 @@ label = customtkinter.CTkLabel(master=app,
                                height=25,
                                fg_color=("white", "gray"),
                                corner_radius=5)
-label.place(relx=0.3, rely=0.5, anchor=tkinter.W)
+label.place(relx=0.5, rely=0.6, anchor=tkinter.W)
 ##n, ne, e, se, s, sw, w, nw, or center
 
 curTime = customtkinter.CTkEntry(master=app,
                                placeholder_text="00:00",
-                               width=120,
+                               width=60,
                                height=25,
                                border_width=2,
                                corner_radius=10)
-curTime.place(relx=0.5, rely=0.5, anchor=tkinter.W)
+curTime.place(relx=0.7, rely=0.6, anchor=tkinter.W)
 
 
 
@@ -152,9 +275,7 @@ combobox = customtkinter.CTkComboBox(master=app,
                                      command=combobox_callback, height = 25, width = 70, variable = "")
 combobox.pack(padx=5, pady=5)
 combobox.set("pm")  # set initial value
-combobox.place(relx=0.75, rely=0.5, anchor=tkinter.W)
+combobox.place(relx=0.81, rely=0.6, anchor=tkinter.W)
 
-button = customtkinter.CTkButton(master=app, text="Quit", command=stop)
-button.place(relx=0.5, rely=.9, anchor=tkinter.CENTER)
 
 app.mainloop()
